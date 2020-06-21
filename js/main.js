0d1616e6f554ec576419ac3adac0f2ac53a38e9b
VladimirMitrofanov <vladimi-mitrofano@yandex.ru>
2020-06-21 14:59:08 +0300

    Оживляем интерактивные элементы

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var mapLink = document.querySelector(".open-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".map-btn");
+var feedbackLink = document.querySelector(".contac-btn");
+var feedbackPopup = document.querySelector(".feedback-popup");
+var feedbackClose = document.querySelector(".feedback-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+feedbackLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add("modal-show");
+});
+
+feedbackClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (feedbackPopup.classList.contains("modal-show")) {
+      feedbackPopup.classList.remove("modal-show");
+    }
+  }
+});
